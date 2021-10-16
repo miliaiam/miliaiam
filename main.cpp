55398ebd050429d70a25f8aa8eea1ed180c9e3bc
miliaiam <92634950+miliaiam@users.noreply.github.com>
2021-10-16 18:22:40 +0400

    надо сделать под себя

--- main.cpp
+++ main.cpp
@@ -1,22 +1,56 @@
+#include "pch.h"
 #include "TXLib.h"
+using namespace std;
+struct menu_button
+{
+	
+	HDC img;
+	COLORREF color= TX_NULL;
+	
+
+
+
+
+};
+bool Button(int x, int y, int w, int h, const char *str);
 
 int main()
-    {
-    txCreateWindow (1000, 700);
+{
+	txCreateWindow(800, 600);
+	
+	while (!GetAsyncKeyState(VK_ESCAPE))
+	{
 
-   
-    txSetColor (TX_WHITE);
-    txSetFillColor (TX_WHITE);
+		txBegin();
+	Button(100, 100, 0, 0, "Одиночная игра");
+	Button(100, 170, 0, 0, "Чемпионат");
+	Button(100, 240, 0, 0, "Настройки");
+	Button(100, 310, 0, 0, "О программе");
+	txEnd();
+	}
+}
 
-    txRectangle(100, 20, 120, 200);
-    txRectangle(140, 20, 160, 200);
-    txRectangle(180, 20, 200, 200);
-    txRectangle(220, 20, 240, 200);
-    txRectangle(260, 20, 280, 200);
-    txRectangle(300, 20, 320, 200);
 
-    return 0;
-    int a;
-    cin>>a;
-    }
+bool Button(int x, int y, int w, int h, const char *str)
+{
 
+	
+	menu_button button1;
+	if (button1.color == TX_NULL)  button1.color = TX_RED;
+	if (w== 0)  w = 120;
+	if (h == 0)  h = 30;
+	
+	txSetFillColor(TX_YELLOW);
+
+	txRectangle(x+3, y+8, x + w+8, y + h+8);
+	txSetFillColor(button1.color);
+	if (txMouseX() > x && txMouseY() > y && txMouseX() < x + w && txMouseY() < y + h) txSetFillColor(TX_GREEN);
+	txRectangle(x, y, x + w, y + h);
+	txSetColor(button1.color, 5);
+	txRectangle(x, y, x+w, y+h);
+	txSetColor(TX_WHITE, 5);
+	txTextOut(x+5, y+5, str);
+
+	return 0;
+
+}
